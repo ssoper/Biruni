daab5f8e6c42fdddb8add4aa8420e8235e9c38f1
ssoper <sean.soper@gmail.com>
2010-12-19 16:56:28 -0500

    adding basic testing

---
+++ UnitTests.h
@@ -0,0 +1,16 @@
+//
+//  UnitTests.h
+//  Biruni
+//
+//  Created by Sean Soper on 12/19/10.
+//  Copyright 2010 Intridea. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface UnitTests : SenTestCase {
+
+}
+
+@end
---
+++ UnitTests.m
@@ -0,0 +1,18 @@
+//
+//  UnitTests.m
+//  Biruni
+//
+//  Created by Sean Soper on 12/19/10.
+//  Copyright 2010 Intridea. All rights reserved.
+//
+
+#import "UnitTests.h"
+#import "Biruni.h"
+
+@implementation UnitTests
+
+- (void) testTrue {
+  STAssertTrue(YES, @"this is true");
+}
+
+@end
